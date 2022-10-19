cb0f9972724ffe874eb9f6eb199ab3c03cb0d73b
VITALY <9004673@mail.ru>
2022-10-19 21:30:43 +0300

    POST Swervlet2

--- out/artifacts/DZ_zayvka_war_exploded/index.jsp
+++ out/artifacts/DZ_zayvka_war_exploded/index.jsp
@@ -1,10 +1,15 @@
-
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
   <head>
     <title>$Title$</title>
   </head>
   <body>
+
+  <c:if test="${alarm == true}">
+    <p>REINPUT</p>
+  </c:if>
 
   <form action="/sPost" method="post">
     Name: <input name="name" />
@@ -15,3 +20,5 @@
   </form>
   </body>
 </html>
+
+
--- src/main/java/servlets/PostServlet.java
+++ src/main/java/servlets/PostServlet.java
@@ -11,19 +11,15 @@ import java.io.IOException;
 public class PostServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ServletContext servletContext = getServletContext();
-        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("web/1.jsp");
-        requestDispatcher.forward(req, resp);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String name = req.getParameter("name");
         String num = req.getParameter("num");
 
-        if((name == "")|(num=="")){ req.getRequestDispatcher("index.jsp").forward(req, resp);
+        if((name == "")||(num=="")){
+
+            req.setAttribute("alarm", true);
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
         }
         else req.getRequestDispatcher("views/1.jsp").forward(req, resp);
 
